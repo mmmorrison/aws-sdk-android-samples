fd01dc061c19d4f5e69d3d51d9584f8a04dbe501
Maria <mmorrison1978@gmail.com>
2016-03-22 15:05:22 -0600

    initial commit

--- S3TransferUtilitySample/src/com/amazonaws/demo/s3transferutility/Constants.java
+++ S3TransferUtilitySample/src/com/amazonaws/demo/s3transferutility/Constants.java
@@ -21,12 +21,12 @@ public class Constants {
      * You should replace these values with your own. See the README for details
      * on what to fill in.
      */
-    public static final String COGNITO_POOL_ID = "CHANGE_ME";
+    public static final String COGNITO_POOL_ID = "us-east-1:6fc2c25b-0e7c-48d6-b1a9-cb4d87dbb622";
 
     /*
      * Note, you must first create a bucket using the S3 console before running
      * the sample (https://console.aws.amazon.com/s3/). After creating a bucket,
      * put it's name in the field below.
      */
-    public static final String BUCKET_NAME = "CHANGE_ME";
+    public static final String BUCKET_NAME = "thisorthatphotofiles";
 }
